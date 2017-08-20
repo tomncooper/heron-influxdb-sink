69d17820374677a6f7a780e0c03d82981d5511da
Thomas Cooper <tom.n.cooper@gmail.com>
2017-08-20 20:06:01 +0100

    Added method stubs

--- src/main/java/uk/org/tomcooper/heronInfluxdb/InfluxSink.java
+++ src/main/java/uk/org/tomcooper/heronInfluxdb/InfluxSink.java
@@ -1,14 +1,32 @@
 package uk.org.tomcooper.heronInfluxdb;
 
 import java.util.Map;
+import java.util.logging.Logger;
+
+import org.influxdb.InfluxDB;
 
 import com.twitter.heron.spi.metricsmgr.metrics.MetricsRecord;
 import com.twitter.heron.spi.metricsmgr.sink.IMetricsSink;
 import com.twitter.heron.spi.metricsmgr.sink.SinkContext;
 
 public class InfluxSink implements IMetricsSink {
+	
+	private static final Logger LOG = Logger.getLogger(InfluxSink.class.getName());
+   
+	/** InluxDB connection instance **/
+	private InfluxDB influxDB;	
+	/** Heron Topology ID string **/
+	private String topologyName;
+	
+	public void init(Map<String, Object> conf, SinkContext context) {
 
-	public void close() {
+		LOG.info("Configuration: " + conf.toString());	
+
+		topologyName = context.getTopologyName();
+	
+	}
+
+	public void processRecord(MetricsRecord arg0) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -18,12 +36,7 @@ public class InfluxSink implements IMetricsSink {
 		
 	}
 
-	public void init(Map<String, Object> arg0, SinkContext arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void processRecord(MetricsRecord arg0) {
+	public void close() {
 		// TODO Auto-generated method stub
 		
 	}

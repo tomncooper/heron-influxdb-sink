d44f663bd16f7bbc53fcc43cf7054ba072865128
Thomas Cooper <tom.n.cooper@gmail.com>
2017-08-20 18:40:07 +0100

    Initial commit

---
+++ src/main/java/uk/org/tomcooper/heronInfluxdb/InfluxSink.java
@@ -0,0 +1,31 @@
+package uk.org.tomcooper.heronInfluxdb;
+
+import java.util.Map;
+
+import com.twitter.heron.spi.metricsmgr.metrics.MetricsRecord;
+import com.twitter.heron.spi.metricsmgr.sink.IMetricsSink;
+import com.twitter.heron.spi.metricsmgr.sink.SinkContext;
+
+public class InfluxSink implements IMetricsSink {
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void init(Map<String, Object> arg0, SinkContext arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void processRecord(MetricsRecord arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
